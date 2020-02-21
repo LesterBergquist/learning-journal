25056a6f6723eedf1193bf965f53f1ff89b82e68
Lester Bergquist <lester@inlinguavancouver.com>
2020-02-20 21:18:39 -0800

    lab 08

---
+++ lab-08.js
@@ -0,0 +1,12 @@
+
+
+var shopping = prompt("How many things would you like to buy?");
+
+var shoppingi =parseInt(shopping)
+
+var items=[]
+for(var i = 0; i <shoppingi; i++){
+    var item = prompt(" Enter Item?");
+    items.push(item)
+
+}
